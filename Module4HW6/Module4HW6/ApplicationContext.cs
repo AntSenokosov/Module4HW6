ec4d1e45973c127dd8f2b4b14b86eb6cf2686096
AntSenokosov <a.y.senokosov@gmail.com>
2022-01-31 19:01:22 +0200

    Add ApplicationContext

---
+++ Module4HW6/Module4HW6/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Module4HW6.Entities;
+using Module4HW6.EntityConfigurations;
+
+namespace Module4HW6;
+
+public class ApplicationContext : DbContext
+{
+    public ApplicationContext(DbContextOptions<ApplicationContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Artist> Artists { get; set; } = null!;
+    public DbSet<Genre> Genres { get; set; } = null!;
+    public DbSet<Song> Songs { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new ArtistConfiguration());
+        modelBuilder.ApplyConfiguration(new GenreConfiguration());
+        modelBuilder.ApplyConfiguration(new SongConfiguration());
+    }
+}
